565fc42b291f80c77b81f5d5ec52cc4d109c9089
agathesf <76791952+agathesf@users.noreply.github.com>
2023-08-04 19:08:57 +0200

    Update development.js

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,15 +1,3 @@
-const { environment } = require('@rails/webpacker')
-const webpack = require('webpack')
-
-environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    Popper: ['popper.js', 'default']
-  })
-)
-
-module.exports = {
-  ...environment.toWebpackConfig(),
-  mode: 'development'
-};
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+const environment = require('./environment')
+module.exports = environment.toWebpackConfig()
